cd3e9ad79a1fc743213e0f6f5eaed8873100b374
Norman Little <norman.little@gmail.com>
2016-06-17 20:16:49 +0100

    First commit adding project structure with DistanceToScore code

---
+++ src/main/java/com/sandbox/DistanceToTheScore.java
@@ -0,0 +1,78 @@
+package com.sandbox;
+
+import static java.lang.Math.abs;
+import static java.lang.String.valueOf;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DistanceToTheScore {
+    public static void main(String[] args) {
+        /*              <Norman>      <Chris>       <Neil>        <Dave>        <Iain>       vs          <result>    */
+        Stream.of(aGame(aScore(1, 0), aScore(1, 0), aScore(2, 0), aScore(1, 0), aScore(1, 1)).withResult(aResult(1, 0)),
+                  aGame(aScore(1, 1), aScore(0, 1), aScore(2, 0), aScore(0, 1), aScore(1, 1)).withResult(aResult(2, 2)),
+                  aGame(aScore(1, 0), aScore(1, 0), aScore(2, 0), aScore(2, 0), aScore(2, 0)).withResult(aResult(0, 0)),
+
+                  aGame(aScore(1, 0), aScore(1, 0), aScore(2, 0), aScore(1, 0), aScore(2, 1)).withResult(aResult(0, 0)),
+                  aGame(aScore(1, 1), aScore(0, 1), aScore(2, 0), aScore(1, 1), aScore(0, 1)).withResult(aResult(0, 0)),
+                  aGame(aScore(1, 0), aScore(1, 1), aScore(2, 0), aScore(2, 0), aScore(2, 0)).withResult(aResult(0, 0)),
+
+                  aGame(aScore(1, 2), aScore(0, 1), aScore(2, 0), aScore(0, 1), aScore(1, 1)).withResult(aResult(0, 0)),
+                  aGame(aScore(1, 0), aScore(1, 1), aScore(2, 0), aScore(1, 1), aScore(1, 0)).withResult(aResult(0, 0)))
+              .map(Game::toResult)
+              .forEach(System.out::println);
+    }
+
+    private static Score aScore(int lhs, int rhs) {
+        return new Score(lhs, rhs);
+    }
+
+    private static Score aResult(int lhs, int rhs) {
+        return aScore(lhs, rhs);
+    }
+
+    public static Game aGame(Score... scores) {
+        return new Game(scores);
+    }
+
+    private static class Score {
+        private int lhs;
+        private int rhs;
+
+        public Score(int lhs, int rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        public int getDistanceFromResult(Score result) {
+            int distance = abs(lhs - result.lhs) + abs(rhs - result.rhs);
+            if ((lhs > rhs && result.lhs > result.rhs) ||
+                    (lhs < rhs && result.lhs < result.rhs) ||
+                    (lhs == rhs && result.lhs == result.rhs)) {
+                // matched result
+            } else {
+                distance++;
+            }
+            return distance;
+        }
+    }
+
+    private static class Game {
+        private Stream<Score> scores;
+        private Score result;
+
+        public Game(Score... scores) {
+            this.scores = Stream.of(scores);
+        }
+
+        public Game withResult(Score result) {
+            this.result = result;
+            return this;
+        }
+
+        public String toResult() {
+            return scores.map(s -> valueOf(s.getDistanceFromResult(result)))
+                    .collect(Collectors.joining(","));
+        }
+    }
+}
