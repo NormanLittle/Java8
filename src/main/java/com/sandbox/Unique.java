dbd1d48b00b253f2d474c02461acf05cc4c8c7a9
Little, Norman <norman.little@sky.uk>
2017-04-18 11:52:54 +0100

    Adding utility for unique id generation

--- src/main/java/com/sandbox/Unique.java
+++ src/main/java/com/sandbox/Unique.java
@@ -1,39 +1,68 @@
 package com.sandbox;
 
+import static com.google.common.collect.Sets.newHashSet;
 import static java.lang.String.format;
+import static java.util.UUID.randomUUID;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.stream.IntStream;
-import org.joda.time.DateTime;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
+
+class IdGenerator implements Callable<String> {
+    private final int count;
+
+    IdGenerator(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String call() throws Exception {
+        String id = format("ROINLP%s",
+                           Long.toString(Math.abs(randomUUID().getLeastSignificantBits()),
+                                         Character.MAX_RADIX));
+
+        System.out.printf("[%03d]:%s%n", count, id);
+        return id;
+    }
+}
 
 public class Unique {
-    private static final int CIPHER[] = {1, 3, 3, 1, 8, 3, 5, 7, 2, 9, 0, 2, 8, 2, 9, 7, 7, 8, 1, 7};
 
-    private static String generateId() {
-        String now = String.valueOf(DateTime.now().getMillis());
-        System.out.println(now);
+    private static List<Future<String>> generateAll(ExecutorService executorService, List<IdGenerator> generators) throws InterruptedException {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        List<Future<String>> results = executorService.invokeAll(generators);
+        System.out.println("Completed in " + stopwatch.stop());
 
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i != now.length(); i++) {
-            builder.append((now.charAt(i) + CIPHER[i]) % 10);
-        }
-        return format("ROINLP%s", builder.toString());
+        return results;
     }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        List<Callable<String>> tasks =
-                IntStream.range(0, 500)
-                         .mapToObj(i -> (Callable<String>) Unique::generateId)
+        ExecutorService executorService = newFixedThreadPool(4);
+
+        List<IdGenerator> generators =
+                IntStream.rangeClosed(0, 100000)
+                         .mapToObj(IdGenerator::new)
                          .collect(toList());
 
-        List<Future<String>> uniqueIds = newFixedThreadPool(500).invokeAll(tasks);
-        for (Future<String> future : uniqueIds) {
-            System.out.println(future.get());
+        List<Future<String>> generatorResults = generateAll(executorService, generators);
+        executorService.shutdownNow();
+
+        Set<String> uniqueIds = newHashSet();
+        for (Future<String> future : generatorResults) {
+            String id = future.get();
+
+            boolean isUnique = uniqueIds.add(id);
+            if (!isUnique) {
+                System.err.printf("%s is not unique.%n", id);
+            }
         }
     }
 }
