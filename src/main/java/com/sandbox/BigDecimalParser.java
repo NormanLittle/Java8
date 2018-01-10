affbae16d74563c99f3196b140dc35320ddcd681
Little, Norman <norman.little@sky.uk>
2018-01-10 16:11:40 +0000

    Trying out BigDecimal parsing

---
+++ src/main/java/com/sandbox/BigDecimalParser.java
@@ -0,0 +1,25 @@
+package com.sandbox;
+
+import static java.math.BigDecimal.ROUND_HALF_EVEN;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BigDecimalParser {
+
+    private static Function<String, BigDecimal> toBigDecimal = s ->
+            new BigDecimal(s)
+                    .divide(new BigDecimal("1000000"), 6, ROUND_HALF_EVEN);
+
+    public static void main(String[] args) {
+        List<BigDecimal> values =
+                Stream.of("55876130", "-3517040", "0")
+                      .map(toBigDecimal)
+                      .collect(Collectors.toList());
+
+        values.forEach(System.out::println);
+    }
+}
